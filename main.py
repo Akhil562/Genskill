6f5a75baa2ecf0e54b9b14970e95d6dabb5b27a2
Akhil562 <akhiljatia234@gmail.com>
2025-07-29 14:53:35 +0530

    final prototype

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ app = Flask(__name__)
 def hello():
     return render_template('welcome.html')
 
+@app.route('/SkillFlip')
+def skillspin():
+    return render_template("skillflip.html")
+
+@app.route('/Suggestions')
+def suggestions():
+    return render_template("suggestions.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,host = '0.0.0.0')
